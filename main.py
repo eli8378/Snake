a2e7403e250ced39bc0831fe9336daaa0930cec3
eli8378 <eli@w2t.io>
2023-10-19 12:55:34 -0500

    added changes to snake movement
    can no longer reverse your self in the direction opposite to what you are going

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ from pygame.math import Vector2
 
 import random as r
 
+import sys
+
 class Snake:
     def __init__(self):
         self.body = [Vector2(5,10), Vector2(6,10), Vector2(7,10)]
-        self.direction = Vector2(1,0)
+        self.direction = Vector2(-1,0)
 
     def drawSnake(self):
         for block in self.body:
@@ -31,43 +33,69 @@ class Fruit:
         fruitRect = pygame.Rect(int(self.pos.x * cellSize),int(self.pos.y * cellSize), cellSize, cellSize)
         pygame.draw.rect(screen, (126,166,114), fruitRect)
 
+class Game:
+    def __init__(self):
+        self.snake = Snake()
+        self.fruit = Fruit()
+
+    def update(self):
+        self.snake.moveSnake()
+        self.checkCollision()
+        self.checkFailure()
+
+    def drawElements(self):
+        self.fruit.drawFruit()
+        self.snake.drawSnake()
+
+    def checkCollision(self):
+        if self.fruit.pos == self.snake.body[0]:
+            self.fruit = Fruit()
+            self.snake.body.append(self.snake.body[-1] + self.snake.direction)
+
+    def checkFailure(self):
+        if not 0 <= self.snake.body[0].x < cellNumber:
+            pygame.quit()
+        elif not 0 <= self.snake.body[0].y < cellNumber:
+            pygame.quit() 
+        for block in self.snake.body[1:]:
+            if block == self.snake.body[0]:
+                pygame.quit()
+
+
 pygame.init()
 cellSize = 40
 cellNumber = 20
 screen = pygame.display.set_mode((cellNumber * cellSize, cellNumber * cellSize))
 pygame.display.set_caption("Snake Game")
 clock = pygame.time.Clock()
-fruit = Fruit()
-snake = Snake()
+game = Game()
 
 SCREEN_UPDATE = pygame.USEREVENT
 pygame.time.set_timer(SCREEN_UPDATE, 150)
 
-def main():
-    keys = pygame.key.get_pressed()
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            if event.type == SCREEN_UPDATE:
-                snake.moveSnake()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
-                    snake.direction = Vector2(0,-1)
-                elif event.key == pygame.K_DOWN:
-                    snake.direction = Vector2(0,1)
-                elif event.key == pygame.K_LEFT:
-                    snake.direction = Vector2(-1,0)
-                elif event.key == pygame.K_RIGHT:
-                    snake.direction = Vector2(1,0)
-
-
-
-        screen.fill((175,215,70))
-        fruit.drawFruit()
-        snake.drawSnake()
-        pygame.display.update()
-        clock.tick(60)
-    pygame.quit()
-main()
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == SCREEN_UPDATE:
+            game.update()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                if game.snake.direction.y != 1:
+                    game.snake.direction = Vector2(0,-1)
+            elif event.key == pygame.K_DOWN:
+                if game.snake.direction.y != -1:
+                    game.snake.direction = Vector2(0,1)
+            elif event.key == pygame.K_LEFT:
+                if game.snake.direction.x != 1:
+                    game.snake.direction = Vector2(-1,0)
+            elif event.key == pygame.K_RIGHT:
+                if game.snake.direction.x != -1:
+                    game.snake.direction = Vector2(1,0)
+                
+    screen.fill((175,215,70))
+    game.drawElements()
+    pygame.display.update()
+    clock.tick(60)
+pygame.quit()
